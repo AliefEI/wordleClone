38beddea6ea3763d31b42cc3cce11b3f75768d1a
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-03-22 14:20:11 -0400

    first commit

---
+++ js/wordle.js
@@ -0,0 +1,13 @@
+/*
+Things to do 
+- have ability to accept input 
+    - create input for entering letters
+    - create a way to erase letter 
+    - create a way to check guesses 
+- creat function to connect guess to vocab api -- or hard code 5-letter words via json
+
+- create function connecting guess to vocabApi function 
+- create 
+
+
+*/
