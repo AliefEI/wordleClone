c45cce5c848f1f6083698d82556e4d547eda0413
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-03-31 17:42:09 -0400

    json package added

--- js/wordle.js
+++ js/wordle.js
@@ -1,13 +1,15 @@
 /*
 Things to do 
-- have ability to accept input 
-    - create input for entering letters
+- have ability to accept input via button 
+    - create input for entering letters --> connect keyboard to button 
     - create a way to erase letter 
     - create a way to check guesses 
 - creat function to connect guess to vocab api -- or hard code 5-letter words via json
-
-- create function connecting guess to vocabApi function 
 - create 
 
 
+
+
+
 */
+
